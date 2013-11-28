cb8aaf0c7f9e840e7eb6babf32d6bb6ecb903ba1
doritadar <dorit.adar@gmail.com>
2013-11-28 11:10:19 -0800

    Update Name.java

--- src/Name.java
+++ src/Name.java
@@ -1,31 +1,40 @@
 import java.util.Scanner;
-
+/**
+ * Each object of class Name represent a name and holds the last name and first name
+ * @author doritadar
+ */
 public class Name {
 	private String lastName;
 	private String firstName;
+	/**
+	 * Constructor method initializes firstName and lastName to Null
+	 */
+	public Name(){
+		lastName = "";
+		firstName = "";
+	}
 	
-public Name(){
-		lastName = null;
-		firstName = null;
-	}
- 
-public void readName(){
-		Scanner NameScanner;
-		NameScanner = new Scanner(System.in);
-		System.out.print("Last name(required):");
-		lastName = NameScanner.nextLine();
-		
-		if(lastName != null){
-			System.out.print("First Name:");
-			firstName = NameScanner.nextLine();
-		}
-		else{
-			System.out.println("Last Name is required to create a new contact, please try again.");
-			readName();
-	}
-}
-public String toString(){
-		return "Name:" + firstName + " " + lastName;
-			
+	/**
+	 * ReadName method reads lastName and firstName from Scanner
+	 * and sets the input into the lastName and firstName variables.
+	 */
+    public void readName(){
+    	Scanner nameScanner = new Scanner(System.in);
+    	System.out.println( "> Enter name"); 
+    	while ("".equals(lastName)){       // check lastName was entered
+    		System.out.print("Last Name (required):");
+    		lastName = nameScanner.nextLine();
+    	}
+		System.out.print("First Name:");
+	    firstName = nameScanner.nextLine();
+	    
+    	}
+    
+    /**
+     * ToString methods formats Name info 
+     */
+	public String toString(){
+		return firstName + " " + lastName;
 	}
 }
+

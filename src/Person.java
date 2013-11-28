ac8b658274b9d9853b2d55b30a655636f5ca9a55
doritadar <dorit.adar@gmail.com>
2013-11-28 11:08:15 -0800

    Update Person.java
    task 3 update

--- src/Person.java
+++ src/Person.java
@@ -1,44 +1,66 @@
 import java.util.Scanner;
 
-
+/**
+ *  Eaah object of class Person represents a person and holds its name, address,
+ *  email, phone number and notes.
+ * @author doritadar
+ *
+ */
 public class Person {
-	private Name name;
-	private StreetAddress streetAddress;
+	private Name name = new Name();
+	private StreetAddress streetAddress = new StreetAddress();
 	private String email;
 	private String phoneNum;
 	private String notes;
-	
+	/**
+	 * The constructor method initializes a person's name,
+	 *  address, email, phone number and notes to null.
+    */
 	public Person(){
-		name = new Name();
-		streetAddress = new StreetAddress();
-		email = null;
-		phoneNum = null;
-		notes = null;
+		
 	}
+	public String getName(){
+		return name.toString();
+	}
+	/**
+	 * The readPerson method reads user input from scanner and 
+	 * sets the input into the name, address, email, phone number and notes variables.
+    */
 	public void readPerson(){
-		//System.out.println( "readPerson method is reached");
 		name.readName();
 		streetAddress.readStreetAddress();
-		Scanner PersonScanner;
-		PersonScanner = new Scanner(System.in);
-		System.out.print("Email:");
-		email = PersonScanner.nextLine();
-		System.out.print("Phone No:");
-		phoneNum = PersonScanner.nextLine();
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Enter Email:");
+	    email = scanner.nextLine();
+		System.out.print("Enter Phone Num:");
+	    phoneNum = scanner.nextLine();
 		System.out.print("Notes:");
-		notes = PersonScanner.nextLine();
+	    notes = scanner.nextLine();
+		}
+	/**
+	 * A method compares this Person to the other Person,
+	 * and returns an int.
+	 * Returns negative int if thisPerson < otherPerson.
+	 * Returns zero if thisPerson == otherPerson.
+	 * Returns positive int if thisPerson > otherPerson.
+	 * @param otherPerson
+	 */
+
+	public int compareTo(Person otherPerson){
+		return 0;
 	}
-	public void compare(){
-		System.out.println("compare method is reached");
-	}
-	
+	/**
+	 * The toString method formats data of a Person object.
+	 */
 	public String toString(){
-		return name.toString()+ "\n"
-				+ "Phone No: " + phoneNum + "\n"
-				+ "Email: " + email+ "\n"
-				+ streetAddress.toString() + "\n"
-				+ "Notes:" + notes;
+		return "\n" + name + "\n" +
+			   "Phone Num: " + phoneNum  + "\n" +
+			   "Email: " + email + "\n" +
+			   "Address: " + streetAddress + "\n"+
+			   "Notes: " + notes + "\n" ; 
 	}
 }
+
+
 
 

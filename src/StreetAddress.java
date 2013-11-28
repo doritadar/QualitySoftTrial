2fbc2c09cbe057a5f017d11ee7ce17880571f15c
doritadar <dorit.adar@gmail.com>
2013-11-28 11:01:30 -0800

    Update StreetAddress.java
    StreetAdreesupdated for case 3, suite changes and toString() checks two cases of adress with or withoutsuite

--- src/StreetAddress.java
+++ src/StreetAddress.java
@@ -1,40 +1,64 @@
 import java.util.Scanner;
+/**
+ * Each object of class StreetAddress represent a street address and holds a 
+ *  house, street, city, zip and country.
+ * @author doritadar
+ *
+ */
 public class StreetAddress {
-	private String house,street,city,state,zip,country;
-	
-public StreetAddress(){
-		house = null; 
-		street = null;
-		city = null;
-		state = null;
-		zip = null;
-		country = null;
+	private String street;
+	private String suite;
+	private String city;
+	private String state;
+	private String zip;
+	private String country;
+	/**
+	 *  The constructor method initializes StreetAddress object's
+	 * street, suite, city, zip and country to null.
+	 */
+	public StreetAddress(){
+	 	street = suite = city = state = zip = country = null;
 	}
- 
-public void readStreetAddress(){
-		
+	/**
+	 * The readStreetAddress method reads user input from Scanner and
+	 * sets the input into the house, street, city, zip and country variables
+	 */
+	public void readStreetAddress(){
+		System.out.println("> Enter address:");
 		Scanner AddressScanner;
-		AddressScanner = new Scanner(System.in);
-		System.out.print("House/Suit:");
-		house = AddressScanner.nextLine();
-		System.out.print("Street:");
-		street = AddressScanner.nextLine();
-		System.out.print("City:");
-		city = AddressScanner.nextLine();
-		System.out.print("State:");
-		state= AddressScanner.nextLine();
-		System.out.print("Zip:");
-		zip = AddressScanner.nextLine();
-		System.out.print("Country:");
-		country = AddressScanner.nextLine();
-		
+	   	AddressScanner = new Scanner(System.in);
+	   	System.out.print("Street:");
+	   	street = AddressScanner.nextLine();
+	   	System.out.print("Suite/Apartment:");
+	    suite = AddressScanner.nextLine();
+	   	System.out.print("City:");
+	   	city = AddressScanner.nextLine();
+	   	System.out.print("State:");
+	   	state= AddressScanner.nextLine();
+	   	System.out.print("Zip:");
+	   	zip = AddressScanner.nextLine();
+	   	System.out.print("Country:");
+	   	country = AddressScanner.nextLine();
+
 	}
-public String toString(){
-		return "Address:" + street 
-				+ " House/Suit " + house + ", " 
-				+ city + ", "
-				+ state + " " + zip + " " + country;
+	/**
+	 * ToString method formats street address info
+	 */
+	public String toString(){
+		if (suite == null){
+			return street + " " +
+				   city + ", " + state + " " + zip + " " +
+				   country + "\n" ;
+		}
+		else{
+			return street + " " + "suite " + suite + " " +
+					city + ", " + state + " " + zip + " " +
+					country ;
+		}
+		
 	}
 }
+
+
 
 
